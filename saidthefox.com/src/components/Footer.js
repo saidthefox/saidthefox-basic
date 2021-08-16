22b50dfdb50cad139367f04408d54a70b6864f4d
saidthefox <35787023+saidthefox@users.noreply.github.com>
2021-08-16 15:39:30 -0500

    edit footer

---
+++ saidthefox.com/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+           <footer>
+                  
+            <div id = "footer">
+                <a href="https://book.saidthefox.com">
+                "It has done me well," said the <fox>fox</fox>, "because of the color of the wheat fields." - Antoine de Saint-Exupery, <i>The Little Prince</i>
+                </a>
+            </div> 
+
+           </footer>
+    )
+}
+
+export default Footer;
