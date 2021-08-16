84caeb59a3b01be67d84592df2959ed8ad82e1d1
saidthefox <35787023+saidthefox@users.noreply.github.com>
2021-08-16 16:23:07 -0500

    Update Body.js

--- saidthefox.com/src/components/Body.js
+++ saidthefox.com/src/components/Body.js
@@ -19,13 +19,7 @@ function Body() {
                       About
                    </button>
                 </a>
-               </div>    
-            <div id = "footer">
-                <a href="https://book.saidthefox.com">
-                "It has done me well," said the <fox>fox</fox>, "because of the color of the wheat fields." - Antoine de Saint-Exupery, <i>The Little Prince</i>
-                </a>
-            </div> 
-
+               </div>
            </body>
     )
 }
